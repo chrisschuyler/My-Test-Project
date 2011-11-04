de6fafc8d44eb492ce04fbe65ce16e1f3110cdd8
cschuyler <cschuyler@labdatasys.com>
2011-11-04 09:27:33 -0400

    Initial Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Configuration;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["WindowsLoginCredentials"]))
+            {
+                VirtualDirectoryMapping vdm = new VirtualDirectoryMapping(@"C:\LDS\Phoenix\PhoenixWeb", true);
+                WebConfigurationFileMap wcfm = new WebConfigurationFileMap();
+                wcfm.VirtualDirectories.Add("/", vdm);
+
+                // Get the Web application configuration object.
+                Configuration config = WebConfigurationManager.OpenMappedWebConfiguration(wcfm, "/");
+
+                string s = config.AppSettings.Settings["WindowsLoginCredentials"].Value;
+            }
+
+            
+
+            Console.Read();
+        }
+
+    }
+}
